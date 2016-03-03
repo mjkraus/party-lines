9e7da54ded36291b6cb2f61533057653390371f0
Molly Kraus <mollyjkraus@gmail.com>
2016-03-03 13:25:39 -0500

    styled quiz results

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -4,35 +4,35 @@
 // http://blog.kevinchisholm.com/javascript/getting-started-with-highcharts-part-i-basic-charts/
   var quoteArray = [{
     topic: "Immigration",
-    quote: "I would secure the borders — north, south, east and west.",
+    quote: '"I would secure the borders — north, south, east and west."',
     speaker: "Ben Carson",
     party: "rep",
     image: "carson.png"
 
   }, {
     topic: "Gun Control",
-    quote: "How many people have to die before we actually act?",
+    quote: '"How many people have to die before we actually act?"',
     speaker: "Hillary Clinton",
     party: "dem",
     image: "clinton.png"    
 
   }, {
     topic: "Health Care",
-    quote: "I don't support Obamacare. I want to repeal it.",
+    quote: '"I do not support Obamacare. I want to repeal it."',
     speaker: "John Kasich",
     party: "rep",
     image: "kasich.png"    
 
   }, {    
     topic: "Same-Sex Marriage",
-    quote: "We should ban discrimination against L.G.B.T. Americans and their families so they can live, learn, marry and work just like everybody else.",
+    quote: '"We should ban discrimination against L.G.B.T. Americans and their families so they can live, learn, marry and work just like everybody else."',
     speaker: "Hillary Clinton",
     party: "dem",
     image: "clinton.png"    
 
   }, {
     topic: "N.S.A",
-    quote: "I assume when I pick up my telephone, people are listening to my conversations anyway, if you want to know the truth. It’s pretty sad commentary. But I err on the side of security.",
+    quote: '"I assume when I pick up my telephone, people are listening to my conversations anyway, if you want to know the truth. It’s pretty sad commentary. But I err on the side of security."',
     speaker: "Donald J Trump",
     party: "rep",
     image: "trump.png"    
@@ -40,35 +40,35 @@
   }, {
 
     topic: "Syrian Refugees",
-    quote: "We can’t background-check them. You can’t pick up the phone and call Syria, and that’s one of the reasons why I’ve said we won’t be able to take more refugees. It’s not that we don’t want to, it’s that we can’t.",
+    quote: '"We can’t background-check them. You can’t pick up the phone and call Syria, and that’s one of the reasons why I’ve said we won’t be able to take more refugees. It’s not that we don’t want to, it’s that we can’t."',
     speaker: "Marco Rubio",
     party: "rep",
     image: "rubio.png"
 
   }, {
     topic: "Dealth Penalty",
-    quote: "The state itself, in a democratic, civilized society, should itself not be involved in the murder of other Americans.",
+    quote: '"The state itself, in a democratic, civilized society, should itself not be involved in the murder of other Americans."',
     speaker: "Bernie Sanders",
     party: "dem",
     image: "sanders.png"    
 
   }, {    
     topic: "Islamic State",
-    quote: "We have got to be as aggressive as we can in destroying ISIS, but we have to learn the lessons of the past, and that means we cannot do it alone.",
+    quote: '"We have got to be as aggressive as we can in destroying ISIS, but we have to learn the lessons of the past, and that means we cannot do it alone."',
     speaker: "Bernie Sanders", 
     party: "dem",
     image: "sanders.png"    
 
   }, {
     topic: "China",
-    quote: "China is on a campaign of territorial expansion, the intent of which is to kick America out of the Pacific.",
+    quote: '"China is on a campaign of territorial expansion, the intent of which is to kick America out of the Pacific."',
     speaker: "Ted Cruz",
     party: "rep",
     image: "cruz.png"    
 
   }, {
     topic: "Climate Change",
-    quote: "Climate change is threatening this planet in horrendous ways.",
+    quote: '"Climate change is threatening this planet in horrendous ways."',
     speaker: "Bernie Sanders",
     party: "dem",
     image: "sanders.png"         
@@ -157,7 +157,8 @@
 
     function displayChoices(c, d){
       for(var i = 0; i < quoteArray.length; i++){
-        var card = $('<div>', {'class': 'card'});
+        var row = $('<div>', {'class': 'row final-results'});
+        var card = $('<div>', {'class': 'col-md-6'});
           if (selections[i] === "agree"){
             var overlay = $('<div>',{'class': 'agree'});
             card.append(overlay);
@@ -165,13 +166,16 @@
             overlay = $('<div>',{'class': 'disagree'});
             card.append(overlay);
           };
+        var card2 = $('<div>', {'class': 'col-md-6'});         
         var image = $('<img>').attr('src','/img/'+quoteArray[i].image);
-        overlay.append(image);        
+        overlay.append(image);
+        row.append(card);
         var quote = $('<p>').append(quoteArray[i].quote);
-        overlay.append(quote);
+        card2.append(quote);
         var speaker = $('<p>').append(quoteArray[i].speaker);
-        overlay.append(speaker);  
-        results.append(card);  
+        card2.append(speaker);
+        row.append(card2);  
+        results.append(row);  
       }
       results.fadeIn();
     }
